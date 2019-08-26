fba849f5e238a33a76842cbee4519f4e3d331cc4
pancholivaibhav1995 <pancholivaibhav@gmail.com>
2019-08-26 22:38:32 +0530

    Initial Commit

---
+++ VB_inventory/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VB_inventory.MvcApplication" Language="C#" %>
---
+++ VB_inventory/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace VB_inventory
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+        public class BundleConfig
+        {
+            public static void RegisterBundles(BundleCollection bundles)
+            {
+                //BundleTable.EnableOptimizations = true;
+                #region "signup"
+                bundles.Add(new ScriptBundle("~/signup.js")
+                       .Include("~/scripts/jquery-3.4.1.min.js")
+                       .Include("~/scripts/popper.min.js")
+                       .Include("~/scripts/bootstrap.min.js")
+                     );
+                #endregion
+            }
+
+        }
+    }
+}
